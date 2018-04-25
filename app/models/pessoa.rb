a527b6e0cefb9cd9f0b39b0463a0752d11d234fe
Gesley Evening Silva Viana <gesley_123@hotmail.com>
2018-04-25 22:26:19 +0000

    Comentado a obrigatoriedade do campo Data de Nascimeno da Pessoa

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,7 +1,7 @@
 class Pessoa < ApplicationRecord
     
     validates_presence_of :nom_pessoa, message: 'Um nome deve ser infomado!'
-    validates_presence_of :dta_nascimento, message: 'Data de nascimento deve ser infomada!'
+    #validates_presence_of :dta_nascimento, message: 'Data de nascimento deve ser infomada!'
     validates_presence_of :num_ano_inicial, message: 'Ano de início deve ser infomado!'
     validates_presence_of :des_endereco, message: 'Endereço deve ser infomado!'
     validates_presence_of :num_celular, message: 'Celular deve ser infomado!'
