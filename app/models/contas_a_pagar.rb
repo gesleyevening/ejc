daded56a9098df03c4fa3fa6bbd55d9e6f6f8a1f
Gesley Evening Silva Viana <gesley_123@hotmail.com>
2018-05-21 14:28:06 +0000

    Feita validações de atributos de campo da aplicação

--- app/models/contas_a_pagar.rb
+++ app/models/contas_a_pagar.rb
@@ -1,7 +1,10 @@
 class ContasAPagar < ApplicationRecord
     
-    validates_presence_of :data_lancamento
-    validates_presence_of :descricao
-    validates_presence_of :valor
+    #validates_presence_of :data_lancamento
+    
+    validates :descricao, presence: {message: 'deve conter uma descrição!'},
+                          length: {in: 10..100, message: 'deve conter entre 10 e 100 caracteres!'}
+                      
+    validates_presence_of :valor, message: 'valor deve ser informado!'
     
 end
