821d9247fb550207d6c18d8eacdfa5c65a35efd4
Pablo Zambaldi <pablozamba@hotmail.com>
2018-06-14 17:06:53 +0000

    Teste

--- app/models/contas_a_pagar.rb
+++ app/models/contas_a_pagar.rb
@@ -1,6 +1,7 @@
 class ContasAPagar < ApplicationRecord
     
     #validates_presence_of :data_lancamento
+
     
     validates :descricao, presence: {message: 'deve conter uma descrição!'},
                           length: {in: 10..100, message: 'deve conter entre 10 e 100 caracteres!'}
