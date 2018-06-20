53cb609afab62cc41f733ac5fe8db8837d109230
Pablo Zambaldi <pablozamba@hotmail.com>
2018-06-20 16:28:38 +0000

    Preparando para entrega

--- app/models/contas_a_pagar.rb
+++ app/models/contas_a_pagar.rb
@@ -5,10 +5,10 @@ class ContasAPagar < ApplicationRecord
     #validates_presence_of :data_lancamento
 
     
-    validates_presence_of :descricao, presence: {message: 'deve conter uma descrição!'},
+    validates :descricao, presence: {message: 'deve conter uma descrição!'},
                           length: {in: 10..100, message: 'deve conter entre 10 e 100 caracteres!'}
                       
-    validates_presence_of :valor, presence: true, :numericality => {:greater_than => 0,  message: 'valor não pode ser zero ou negativo!' }
+    validates :valor, presence: true, :numericality => {:greater_than => 0,  message: 'valor não pode ser zero ou negativo!' }
     
     
 end
