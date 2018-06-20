e71f2974648df523bb459e920ce734360d3b1bdd
Pablo Dias <pablo_dias@live.com>
2018-06-20 15:59:33 +0000

    Testes unitarios

--- spec/models/tio_spec.rb
+++ spec/models/tio_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Tio, type: :model do
   it "é valido" do
       i = Tio.new
       i.nome = "Tiosfhsfsdfsdvdjfskjabkbs"
-      i.ano_inicio = "2018-12-1"
+      i.ano_inicio = "2018"
       i.celular = "31987446523"
       i.email = "teste@email.com"
       expect(i).to be_valid
@@ -12,7 +12,7 @@ RSpec.describe Tio, type: :model do
   it "é invalido sem nome" do
       i = Tio.new
       i.nome = nil
-      i.ano_inicio = "2018-12-1"
+      i.ano_inicio = "2018"
       i.celular = "31987446523"
       i.email = "teste@email.com"
       expect(i).to_not be_valid
@@ -28,7 +28,7 @@ RSpec.describe Tio, type: :model do
   it "é invalido sem celular" do
       i = Tio.new
       i.nome = "Tiosfhsfsdfsdvdjfskjabkbs"
-      i.ano_inicio = "2018-12-1"
+      i.ano_inicio = "2018"
       i.celular = nil
       i.email = "teste@email.com"
       expect(i).to_not be_valid
@@ -36,7 +36,7 @@ RSpec.describe Tio, type: :model do
   it "é invalido sem email" do
       i = Tio.new
       i.nome = "Tiosfhsfsdfsdvdjfskjabkbs"
-      i.ano_inicio = "2018-12-1"
+      i.ano_inicio = "2018"
       i.celular = "31987446523"
       i.email = nil
       expect(i).to_not be_valid
